2342526a3bbf2e1adf8e82021826a630a57c4b21
djfalciani <dfalciani@gmail.com>
2019-08-24 16:30:41 -0400

    trying to use env variables, not working at moment

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,14 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+require("dotenv").config();
+
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
 
 // Make connection.
