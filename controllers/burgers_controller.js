ccb20111e83a7185801199d02cd544b20e3ca6f6
djfalciani <dfalciani@gmail.com>
2019-10-18 17:13:52 -0400

    Updated controller to work with Burgers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
+// Import the model (burger.js) to use its database functions.
 const burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
     burger.selectAll(function(data) {
         const hbsObject = {
-            cats: data
+            burger: data
         };
         console.log(hbsObject);
         res.render("index", hbsObject);
